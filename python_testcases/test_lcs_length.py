0cdc1a59d62e418b24b7e8b68cb3d6131d705201
Aaryaman Bisht <aaryaman.bisht2006@gmail.com>
2025-05-29 21:17:09 +0530

    Add files via upload

---
+++ python_testcases/test_lcs_length.py
@@ -0,0 +1,16 @@
+import pytest
+from load_testdata import load_json_testcases
+
+if pytest.use_correct:
+    from correct_python_programs.lcs_length import lcs_length
+elif pytest.fixed:
+    from fixed_programs.lcs_length import lcs_length
+else:
+    from python_programs.lcs_length import lcs_length
+
+
+testdata = load_json_testcases(lcs_length.__name__)
+
+@pytest.mark.parametrize("input_data,expected", testdata)
+def test_lcs_length(input_data, expected):
+    assert lcs_length(*input_data) == expected
